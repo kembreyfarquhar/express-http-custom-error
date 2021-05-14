16877972162aaa78c303dee6a7c8e2b224a79576
kembreyfarquhar <kembreyfarquhar@gmail.com>
2021-05-13 20:52:10 -0500

    adds psql error handling

--- src/errorHandler.js
+++ src/errorHandler.js
@@ -6,8 +6,16 @@ function errorHandler(err, _req, res, _next) {
 	if (err instanceof HTTPError) {
 		response = err.toJSON();
 	} else {
-		const genericError = new InternalServerError();
-		response = genericError.toJSON();
+		for (let key in err) {
+			if (key === 'code' && Object.keys(err).includes('detail')) {
+				const error = new InternalServerError(err.detail);
+				response = error.toJSON();
+			}
+		}
+		if (!response) {
+			const genericError = new InternalServerError();
+			response = genericError.toJSON();
+		}
 	}
 	res.status(response.statusCode).json(response);
 	errorLogger(err, response);
